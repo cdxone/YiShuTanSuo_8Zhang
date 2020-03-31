06306633336f0d5d25b9d06e6d2b9dcf6dc52263
chendexiang <937682896@qq.com>
2020-03-31 14:00:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yishutansuo_8zhang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yishutansuo_8zhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
